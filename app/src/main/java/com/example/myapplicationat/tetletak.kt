de91a1839cc8fc02e5214af50247c69e9b3d9c9e
firzywijatmiko <emailkamu@example.com>
2025-10-17 16:19:23 +0700

    mengubah fontwiget

--- app/src/main/java/com/example/myapplicationat/tetletak.kt
+++ app/src/main/java/com/example/myapplicationat/tetletak.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -167,6 +168,28 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 color = Color.Red,
             )
         }
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center)
+            )
     }
 }
 
