ec7b5d6f36ebc79b798a3e250ca9223e04da2ed9
firzywijatmiko <emailkamu@example.com>
2025-10-17 15:32:14 +0700

    menambahkan fungtion tataletakcolum

--- app/src/main/java/com/example/myapplicationat/tetletak.kt
+++ app/src/main/java/com/example/myapplicationat/tetletak.kt
@@ -1,9 +1,15 @@
 package com.example.myapplicationat
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -16,6 +22,51 @@ fun TataletakColumn(modifier: Modifier) {
         Text(text = "Komponen4")
     }
 }
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
+@Composable
+fun TataletakColumnRow(modifier: Modifier) {
+    Column {
+        // Baris 1
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = "Komponen1")
+            Text(text = "Komponen2")
+            Text(text = "Komponen3")
+            Text(text = "Komponen4")
+        }
+    }
+}
+
 
 
 
